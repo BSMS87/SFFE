8f9e8cea63a2e900d086f8235e0e0a77d2a09d26
FKMBB <77534073+FKMBB@users.noreply.github.com>
2021-02-15 22:23:45 +0100

    Update FK_Code.R

--- FK_Code.R
+++ FK_Code.R
@@ -1,132 +1,133 @@
+####Code-Projektarbeit Wissenschaftliche Methodik
+
 #Libraries setzen
 library(haven)
 library(foreign)
 library(mosaic)
 
-#Daten einlesen
-q <- read_spss("ZA6702_v1-0-0.sav")
-#q <- read_saw("ZA6702_v1-0-0.sav", use.value.labels=TRUE, max.value.labels=Inf, to.data.frame=TRUE)
-colnames(q) <- tolower(colnames(q))
+#Data Source (ds) - Datei einlesen
+ds <- read_sav("ZA6702_v1-0-0.sav")#, use.value.labels=TRUE, max.value.labels=Inf, to.data.frame=TRUE)
+colnames(ds) <- tolower(colnames(ds))
 
-#Auf Gesch‰ftsf¸hrer einschr‰nken --> Self-employed/ the owner of the business
-q <- q[q$q29==1]
+#Auf Gesch√§ftsf√ºhrer einschr√§nken --> Self-employed/ the owner of the business
+ds <- ds[ds$q29==1]
 
 ##Frage 46 - alle Daten hierzu ausgeben lassen
-tally(q$q46)
-str(q$q46)
-sort(table(q$q46)) #Werte sortiert ausgeben
+tally(ds$q46)
+str(ds$q46)
+sort(table(ds$q46)) #Werte sortiert ausgeben
 
 ##Frage 46 - Werte als Tabelle ausgeben
-tabq$q46 <- table(q$q46)
+tabq$q46 <- table(ds$q46)
 View(tabq$q46)
 
 #Kategorien bestimmen - Frage 46
-q$Berufsausbildung [q$q46==-1] <- "N/A"
-q$Berufsausbildung[q$q46==1] <- "Prof. Lever"
-q$Berufsausbildung  [q$q46==2] <- "Middle Level"
-q$Berufsausbildung  [q$q46==3] <- "Junior Level"
-q$Berufsausbildung  [q$q46==4] <- "forewoman"
-q$Berufsausbildung  [q$q46==5] <- "skilled worker"
-q$Berufsausbildung  [q$q46==6] <- "semi-skilled worker"
-q$Berufsausbildung  [q$q46==7] <- "unskilled worker"
-q$Berufsausbildung  [q$q46==8] <- "Agricultural worker"
-q$Berufsausbildung  [q$q46==9] <- "Categories N/A"
-tally(q$Berufsausbildung)
+ds$Berufsausbildung [ds$q46==-1] <- "N/A"
+ds$Berufsausbildung[ds$q46==1] <- "Prof. Lever"
+ds$Berufsausbildung  [ds$q46==2] <- "Middle Level"
+ds$Berufsausbildung  [ds$q46==3] <- "Junior Level"
+ds$Berufsausbildung  [ds$q46==4] <- "forewoman"
+ds$Berufsausbildung  [ds$q46==5] <- "skilled worker"
+ds$Berufsausbildung  [ds$q46==6] <- "semi-skilled worker"
+ds$Berufsausbildung  [ds$q46==7] <- "unskilled worker"
+ds$Berufsausbildung  [ds$q46==8] <- "Agricultural worker"
+ds$Berufsausbildung  [ds$q46==9] <- "Categories N/A"
+tally(ds$Berufsausbildung)
 
 #Umbauen der Kategorien nach White / Blue Colour / Berufsausbildung - Frage 46
-q$Berufsausbildung [q$q46==1] <- "Professional Level"
-q$Berufsausbildung [q$q46==2] <- "Middle Level" 
-q$Berufsausbildung [q$q46==3] <- "Low/Junior/Trainee Level"
-q$Berufsausbildung [q$q46==4] <- "Middle Level"
-q$Berufsausbildung [q$q46==5] <- "Professional Level" 
-q$Berufsausbildung [q$q46==6] <- "Middle Level" 
-q$Berufsausbildung [q$q46==7] <- "Low/Junior/Trainee Level" 
-tally(q$Berufsausbildung)
+ds$Berufsausbildung [ds$q46==1] <- "Professional Level"
+ds$Berufsausbildung [ds$q46==2] <- "Middle Level" 
+ds$Berufsausbildung [ds$q46==3] <- "Low/Junior/Trainee Level"
+ds$Berufsausbildung [ds$q46==4] <- "Middle Level"
+ds$Berufsausbildung [ds$q46==5] <- "Professional Level" 
+ds$Berufsausbildung [ds$q46==6] <- "Middle Level" 
+ds$Berufsausbildung [ds$q46==7] <- "Low/Junior/Trainee Level" 
+tally(ds$Berufsausbildung)
 
 #Spalten der Reihenfolge entsprechend aufbereiten - Frage 46
-q$Berufsausbildung <- factor(q$Berufsausbildung, levels = c("Low/Junior/Trainee Level","Middle Level","Professional Level","N/A"))
-tally(q$Berufsausbildung)
+ds$Berufsausbildung <- factor(ds$Berufsausbildung, levels = c("Low/Junior/Trainee Level","Middle Level","Professional Level","N/A"))
+tally(ds$Berufsausbildung)
 
-#S‰ulendiagramm darstellen - Frage 46
-tabBerufsausbildung <- table(q$Berufsausbildung)
+#S√§ulendiagramm darstellen - Frage 46
+tabBerufsausbildung <- table(ds$Berufsausbildung)
 barplot(tabBerufsausbildung,xlab= 'Categories', cex.names = 0.6, ylab='Amount Frequency')
 
 ##GF_Boxplot erstellen
 
 #Umbauen der Kategorien nach White / Blue Colour / Berufsausbildung mit entsprechenden Zahlen - Frage 46
-q$Berufsausbildung [q$q46==1] <- 3 #"Professional Level"
-q$Berufsausbildung [q$q46==2] <- 2 #"Middle Level" 
-q$Berufsausbildung [q$q46==3] <- 1 #"Low/Junior/Trainee Level"
-q$Berufsausbildung [q$q46==4] <- 2 #"Middle Level"
-q$Berufsausbildung [q$q46==5] <- 3 #"Professional Level" 
-q$Berufsausbildung [q$q46==6] <- 2 #"Middle Level" 
-q$Berufsausbildung [q$q46==7] <- 1 #"Low/Junior/Trainee Level" 
-tally(q$Berufsausbildung)
+ds$Berufsausbildung [ds$q46==1] <- 3 #"Professional Level"
+ds$Berufsausbildung [ds$q46==2] <- 2 #"Middle Level" 
+ds$Berufsausbildung [ds$q46==3] <- 1 #"Low/Junior/Trainee Level"
+ds$Berufsausbildung [ds$q46==4] <- 2 #"Middle Level"
+ds$Berufsausbildung [ds$q46==5] <- 3 #"Professional Level" 
+ds$Berufsausbildung [ds$q46==6] <- 2 #"Middle Level" 
+ds$Berufsausbildung [ds$q46==7] <- 1 #"Low/Junior/Trainee Level" 
+tally(ds$Berufsausbildung)
 
 #Als Tabelle aufbereiten - Frage 46
-tabBerufsausbildung <- table(q$Berufsausbildung)
+tabBerufsausbildung <- table(ds$Berufsausbildung)
 View(tabquestion46_with_Category)
 
 #Kategorien bestimmen - Frage 29
-q$Besch‰ftigungsstatus [q$q29==1] <- "self-employed"
-q$Besch‰ftigungsstatus  [q$q29==2] <- "paid/unpaid family Member"
-q$Besch‰ftigungsstatus  [q$q29==3] <- "paid/unpaid family Member"
-q$Besch‰ftigungsstatus  [q$q29==4] <- "employed"
-tally(q$Besch‰ftigungsstatus)
+ds$Besch√§ftigungsstatus [ds$q29==1] <- "self-employed"
+ds$Besch√§ftigungsstatus  [ds$q29==2] <- "paid/unpaid family Member"
+ds$Besch√§ftigungsstatus  [ds$q29==3] <- "paid/unpaid family Member"
+ds$Besch√§ftigungsstatus  [ds$q29==4] <- "employed"
+tally(ds$Besch√§ftigungsstatus)
 
 #Spalten der Reihenfolge entsprechend aufbereiten - Frage 29
-#q$Besch‰ftigungsstatus<- factor(q$Besch‰ftigungsstatus, levels = c(""))
-#tally(q$Besch‰ftigungsstatus)
+#ds$Besch√§ftigungsstatus<- factor(ds$Besch√§ftigungsstatus, levels = c(""))
+#tally(ds$Besch√§ftigungsstatus)
 
 #Als Tabelle aufbereiten - Frage 29
-tabBesch‰ftigungsstatus <- table(q$Besch‰ftigungsstatus)
-View(tabBesch‰ftigungsstatus)
+tabBesch√§ftigungsstatus <- table(ds$Besch√§ftigungsstatus)
+View(tabBesch√§ftigungsstatus)
 
-#S‰ulendiagramm darstellen - Frage 29
-barplot(tabBesch‰ftigungsstatus,xlab= 'Categories', cex.names = 0.6, ylab='Amount Frequency')
+#S√§ulendiagramm darstellen - Frage 29
+barplot(tabBesch√§ftigungsstatus,xlab= 'Categories', cex.names = 0.6, ylab='Amount Frequency')
 
 #Zufriedenheit finanzieller Status - Frage 61
-ZufriedenheitStatus <- q$q61_1
-tally(q$q61_1)
+ZufriedenheitStatus <- ds$q61_1
+tally(ds$q61_1)
 
 #Zufriedenheit finanzieller Status - Zuordnung und Konsolidierung - Frage 61
-q$ZufriedenheitStatus [q$q61_1==1] <- "sehr unzufrieden"
-q$ZufriedenheitStatus [q$q61_1==2] <- "sehr unzufrieden"
-q$ZufriedenheitStatus [q$q61_1==3] <- "unzufrieden"
-q$ZufriedenheitStatus [q$q61_1==4] <- "unzufrieden"
-q$ZufriedenheitStatus [q$q61_1==5] <- "neutral"
-q$ZufriedenheitStatus [q$q61_1==6] <- "neutral"
-q$ZufriedenheitStatus [q$q61_1==7] <- "zufrieden"
-q$ZufriedenheitStatus [q$q61_1==8] <- "zufrieden"
-q$ZufriedenheitStatus [q$q61_1==9] <- "sehr zufrieden"
-q$ZufriedenheitStatus [q$q61_1==10] <- "sehr zufrieden"
-q$ZufriedenheitStatus [q$q61_1==98] <- "N/A"
-q$ZufriedenheitStatus [q$q61_1==99] <- "N/A"
+ds$ZufriedenheitStatus [ds$q61_1==1] <- "sehr unzufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==2] <- "sehr unzufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==3] <- "unzufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==4] <- "unzufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==5] <- "neutral"
+ds$ZufriedenheitStatus [ds$q61_1==6] <- "neutral"
+ds$ZufriedenheitStatus [ds$q61_1==7] <- "zufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==8] <- "zufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==9] <- "sehr zufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==10] <- "sehr zufrieden"
+ds$ZufriedenheitStatus [ds$q61_1==98] <- "N/A"
+ds$ZufriedenheitStatus [ds$q61_1==99] <- "N/A"
 
 ##Boxplot/Mosaicplot/XChi/Prozentabelle/...ausgeben
 
 #Boxplot auf Frage 29/Frage 46
-gf_boxplot(Berufsausbildung~Besch‰ftigungsstatus, data=q)
+gf_boxplot(Berufsausbildung~Besch√§ftigungsstatus, data=ds)
 
-#Boxplot auf Frage 29/Frage 46/Frage 61 -> Geht das ¸berhaupt?
-#gf_boxplot(Berufsausbildung~Besch‰ftigungsstatus~ZufriedenheitStatus, data=q)
+#Boxplot auf Frage 29/Frage 46/Frage 61 -> Geht das √ºberhaupt?
+#gf_boxplot(Berufsausbildung~Besch√§ftigungsstatus~ZufriedenheitStatus, data=ds)
 
 #Mosaic auf Frage 29/Frage 46 
-mosaicplot(Berufsausbildung~Besch‰ftigungsstatus, data=q)
-mosaicplot(Besch‰ftigungsstatus~Berufsausbildung, data=q)
+mosaicplot(Berufsausbildung~Besch√§ftigungsstatus, data=ds)
+mosaicplot(Besch√§ftigungsstatus~Berufsausbildung, data=ds)
 
 #Chitest auf Frage 29/Frage 46 
-xchisq.test(Berufsausbildung~Besch‰ftigungsstatus, data=q)
+xchisq.test(Berufsausbildung~Besch√§ftigungsstatus, data=ds)
 
 #Tabellenausgabe auf Frage 29/Frage 46 
-tally(Berufsausbildung~Besch‰ftigungsstatus, data=q)
+tally(Berufsausbildung~Besch√§ftigungsstatus, data=ds)
 
 #Prozentausgabe auf Frage 29/Frage 46
-tally(Berufsausbildung~Besch‰ftigungsstatus, data=q, format="percent", useNA="no")
+tally(Berufsausbildung~Besch√§ftigungsstatus, data=ds, format="percent", useNA="no")
 
 #gf_jiterausgabe auf Frage 29/Frage 46 
-gf_jitter(Berufsausbildung~Besch‰ftigungsstatus, data=q)
+gf_jitter(Berufsausbildung~Besch√§ftigungsstatus, data=ds)
 
 #Korrelationausgabe auf Frage 29/Frage 46
-q$Besch‰ftigungsstatus <- as.numeric(q$Besch‰ftigungsstatus)
-cor.test(Berufsausbildung~Besch‰ftigungsstatus, data=q)
+ds$Besch√§ftigungsstatus <- as.numeric(ds$Besch√§ftigungsstatus)
+cor.test(Berufsausbildung~Besch√§ftigungsstatus, data=ds)

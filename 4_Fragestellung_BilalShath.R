6b8b21d9476f40d4262a91a6af660aa3748c4632
BShath <BSMS@Bilals-MacBook-Pro.local>
2021-02-26 00:02:58 +0100

    Updating the test code

--- 4_Fragestellung_BilalShath.R
+++ 4_Fragestellung_BilalShath.R
@@ -1,8 +1,9 @@
 library(mosaic)
 library(haven)
-library(tidyverse)
 
 ds <- read_sav("ZA6702_v1-0-0.sav")
+
+# Spaltennamen auf Kleinbuchstaben umsetzen
 colnames(ds) <- tolower(colnames(ds))
 
 ## Dataframe bewerten
@@ -13,87 +14,88 @@ str(ds)
 print(ds)
 print(summary(ds))
 
-### Erste Fragestellung: Spielt die Familie oder die Freunden eine Rolle für den Geschäftserfolg?
+### Erste Fragestellung: Spielen Familie oder Freunde eine Rolle für den Geschäftserfolg?​?
 
 # Wichtigkeit der Familie - Frage q1_1
 ds$q1_1
 tally(ds$q1_1)
 
-ds$WichtigkeitFamilie[ds$q1_1==1] <- "Sehr Wichtig"
-ds$WichtigkeitFamilie[ds$q1_1==2] <- "Ziemlich Wichtig"
-ds$WichtigkeitFamilie[ds$q1_1==3] <- "Nicht Wichtig"
-ds$WichtigkeitFamilie[ds$q1_1==4] <- "Nicht Wichtig"
-ds$WichtigkeitFamilie[ds$q1_1==8] <- "Neutral"
-ds$WichtigkeitFamilie[ds$q1_1==9] <- "N/A"
-ds$WichtigkeitFamilie
+ds$wichtigkeit_familie[ds$q1_1==1] <- "Sehr Wichtig"
+ds$wichtigkeit_familie[ds$q1_1==2] <- "Ziemlich Wichtig"
+ds$wichtigkeit_familie[ds$q1_1==3] <- "Nicht Wichtig"
+ds$wichtigkeit_familie[ds$q1_1==4] <- "Nicht Wichtig"
+ds$wichtigkeit_familie[ds$q1_1==8] <- "Neutral"
+ds$wichtigkeit_familie[ds$q1_1==9] <- "N/A"
+ds$wichtigkeit_familie
 
 # Sortieren der Werte
-ds$WichtigkeitFamilie <- factor(ds$WichtigkeitFamilie, levels = c ("Sehr Wichtig","Ziemlich Wichtig","Nicht Wichtig","Neutral","N/A"))
+ds$wichtigkeit_familie <- factor(ds$wichtigkeit_familie, levels = c ("Sehr Wichtig","Ziemlich Wichtig","Nicht Wichtig","Neutral","N/A"))
 
 # überprüfen 
-tally(ds$WichtigkeitFamilie)
-typeof(ds$WichtigkeitFamilie)
+tally(ds$wichtigkeit_familie)
+typeof(ds$wichtigkeit_familie)
 
 
 # Graph - Wichtigkeit der Familie
-gf_counts(~ WichtigkeitFamilie, data = ds, fill = ~ WichtigkeitFamilie, position = position_dodge())
+gf_counts(~ wichtigkeit_familie, data = ds, fill = ~ wichtigkeit_familie, position = position_dodge())
 
 # Zufriedenheit der Finanziellen Status - Frage q61_1
 ds$q61_1
 tally(ds$q61_1)
-ds$ZufriedenheitStatus[ds$q61_1==1] <- "sehr unzufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==2] <- "sehr unzufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==3] <- "unzufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==4] <- "unzufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==5] <- "neutral"
-ds$ZufriedenheitStatus[ds$q61_1==6] <- "neutral"
-ds$ZufriedenheitStatus[ds$q61_1==7] <- "zufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==8] <- "zufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==9] <- "sehr zufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==10] <- "sehr zufrieden"
-ds$ZufriedenheitStatus[ds$q61_1==98] <- "N/A"
-ds$ZufriedenheitStatus[ds$q61_1==99] <- "N/A"
+ds$zufriedenheit_status[ds$q61_1==1] <- "sehr unzufrieden"
+ds$zufriedenheit_status[ds$q61_1==2] <- "sehr unzufrieden"
+ds$zufriedenheit_status[ds$q61_1==3] <- "unzufrieden"
+ds$zufriedenheit_status[ds$q61_1==4] <- "unzufrieden"
+ds$zufriedenheit_status[ds$q61_1==5] <- "neutral"
+ds$zufriedenheit_status[ds$q61_1==6] <- "neutral"
+ds$zufriedenheit_status[ds$q61_1==7] <- "zufrieden"
+ds$zufriedenheit_status[ds$q61_1==8] <- "zufrieden"
+ds$zufriedenheit_status[ds$q61_1==9] <- "sehr zufrieden"
+ds$zufriedenheit_status[ds$q61_1==10] <- "sehr zufrieden"
+ds$zufriedenheit_status[ds$q61_1==98] <- "N/A"
+ds$zufriedenheit_status[ds$q61_1==99] <- "N/A"
 
 # Sortieren der Werte
-ds$ZufriedenheitStatus <- factor(ds$ZufriedenheitStatus, levels = c ("sehr unzufrieden","unzufrieden","neutral","zufrieden","sehr zufrieden","N/A"))
-tally(ds$ZufriedenheitStatus)
-typeof(ds$ZufriedenheitStatus)
+ds$zufriedenheit_status <- factor(ds$zufriedenheit_status, levels = c ("sehr unzufrieden","unzufrieden","neutral","zufrieden","sehr zufrieden","N/A"))
+tally(ds$zufriedenheit_status)
+typeof(ds$zufriedenheit_status)
 
 # Graph - Wichtigkeit der Familie mit abhängigkeit der zufriedenheit
-gf_jitter(ZufriedenheitStatus~WichtigkeitFamilie, data = ds)
-gf_counts(~ ZufriedenheitStatus, data = ds, fill = ~ WichtigkeitFamilie, position = position_dodge())
+gf_jitter(zufriedenheit_status~wichtigkeit_familie, data = ds)
+gf_counts(~ wichtigkeit_familie, data = ds, fill = ~ zufriedenheit_status , position = position_dodge())
 
-# Übersicht auf die Prozentuale Verteilung der Wichtigkeit von Familie mit abhängigkeit der zufriedenheit
-tally(WichtigkeitFamilie ~ ZufriedenheitStatus, data=ds, format = "percent")
+Mod1 <- aov(q61_1 ~ q1_1, data = ds)
+summary(Mod1)
 
 
 # Wichtigkeit der Freunde - Frage q1_2
 ds$q1_2
 tally(ds$q1_2)
-ds$WichtigkeitFreunde[ds$q1_2==1] <- "Sehr Wichtig"
-ds$WichtigkeitFreunde[ds$q1_2==2] <- "Ziemlich Wichtig"
-ds$WichtigkeitFreunde[ds$q1_2==3] <- "Nicht Wichtig"
-ds$WichtigkeitFreunde[ds$q1_2==4] <- "Nicht Wichtig"
-ds$WichtigkeitFreunde[ds$q1_2==8] <- "Neutral"
-ds$WichtigkeitFreunde[ds$q1_2==9] <- "N/A"
-ds$WichtigkeitFreunde
+ds$wichtigkeit_freunde[ds$q1_2==1] <- "Sehr Wichtig"
+ds$wichtigkeit_freunde[ds$q1_2==2] <- "Ziemlich Wichtig"
+ds$wichtigkeit_freunde[ds$q1_2==3] <- "Nicht Wichtig"
+ds$wichtigkeit_freunde[ds$q1_2==4] <- "Nicht Wichtig"
+ds$wichtigkeit_freunde[ds$q1_2==8] <- "Neutral"
+ds$wichtigkeit_freunde[ds$q1_2==9] <- "N/A"
+ds$wichtigkeit_freunde
 
 # Sortieren der Werte
-ds$WichtigkeitFreunde <- factor(ds$WichtigkeitFreunde, levels = c ("Sehr Wichtig","Ziemlich Wichtig","Nicht Wichtig","Neutral","N/A"))
+ds$wichtigkeit_freunde <- factor(ds$wichtigkeit_freunde, levels = c ("Sehr Wichtig","Ziemlich Wichtig","Nicht Wichtig","Neutral","N/A"))
 
 # überprüfen 
-tally(ds$WichtigkeitFreunde)
-typeof(ds$ZufriedenheitStatus)
+tally(ds$wichtigkeit_freunde)
+typeof(ds$zufriedenheit_status)
 
 # Graph erstellen - Wichtigkeit der Freunde 
-gf_counts(~ WichtigkeitFreunde, data = ds, fill = ~ WichtigkeitFreunde, position = position_dodge())
+gf_counts(~ wichtigkeit_freunde, data = ds, fill = ~ wichtigkeit_freunde, position = position_dodge())
 
 # Graph - Wichtigkeit der Freunde mit abhängigkeit der zufriedentheit 
-gf_jitter(ZufriedenheitStatus~WichtigkeitFreunde, data = ds)
-gf_counts(~ ZufriedenheitStatus, data = ds, fill = ~ WichtigkeitFreunde, position = position_dodge())
+gf_jitter(zufriedenheit_status~wichtigkeit_freunde, data = ds)
+gf_counts(~ wichtigkeit_freunde, data = ds, fill = ~ zufriedenheit_status, position = position_dodge())
 
-# Übersicht auf die Prozentuale Verteilung der Wichtigkeit von Freunde mit abhängigkeit der zufriedenheit
-tally(WichtigkeitFreunde ~ ZufriedenheitStatus, data=ds, format = "percent")
+Mod2 <- aov(q61_1 ~ q1_2, data = ds)
+summary(Mod2)
+
 
 ### Test - Wichtigkeit der Familie mit abhängigkeit der zufriedenheit
 cor.test(q61_1~q1_1, data = ds)
@@ -109,24 +111,27 @@ xchisq.test(q61_1~q1_1, data = ds)
 # Wichtigkeit der Freizeit - Frage q1_3
 ds$q1_3
 tally(ds$q1_3)
-ds$WichtigkeitFreizeit[ds$q1_3==1] <- "Sehr Wichtig"
-ds$WichtigkeitFreizeit[ds$q1_3==2] <- "Ziemlich Wichtig"
-ds$WichtigkeitFreizeit[ds$q1_3==3] <- "Nicht Wichtig"
-ds$WichtigkeitFreizeit[ds$q1_3==4] <- "Nicht Wichtig"
-ds$WichtigkeitFreizeit[ds$q1_3==8] <- "Neutral"
-ds$WichtigkeitFreizeit[ds$q1_3==9] <- "N/A"
-ds$WichtigkeitFreizeit
+ds$wichtigkeit_freizeit[ds$q1_3==1] <- "Sehr Wichtig"
+ds$wichtigkeit_freizeit[ds$q1_3==2] <- "Ziemlich Wichtig"
+ds$wichtigkeit_freizeit[ds$q1_3==3] <- "Nicht Wichtig"
+ds$wichtigkeit_freizeit[ds$q1_3==4] <- "Nicht Wichtig"
+ds$wichtigkeit_freizeit[ds$q1_3==8] <- "Neutral"
+ds$wichtigkeit_freizeit[ds$q1_3==9] <- "N/A"
+ds$wichtigkeit_freizeit
 
 # Sortieren der Werte
-ds$WichtigkeitFreizeit <- factor(ds$WichtigkeitFreizeit, levels = c ("Sehr Wichtig","Ziemlich Wichtig","Nicht Wichtig","Neutral","N/A"))
+ds$wichtigkeit_freizeit <- factor(ds$wichtigkeit_freizeit, levels = c ("Sehr Wichtig","Ziemlich Wichtig","Nicht Wichtig","Neutral","N/A"))
 
 # überprüfen 
-tally(ds$WichtigkeitFreizeit)
-typeof(ds$WichtigkeitFreizeit)
+tally(ds$wichtigkeit_freizeit)
+typeof(ds$wichtigkeit_freizeit)
 
 # Graph - Wichtigkeit der Freizeit 
-gf_jitter(ZufriedenheitStatus~WichtigkeitFreizeit, data = ds)
-gf_counts(~ ZufriedenheitStatus, data = ds, fill = ~ WichtigkeitFreizeit, position = position_dodge())
+gf_jitter(zufriedenheit_status~wichtigkeit_freizeit, data = ds)
+gf_counts(~ wichtigkeit_freizeit, data = ds, fill = ~ zufriedenheit_status , position = position_dodge())
+
+Mod3 <- aov(q61_1 ~ q1_3, data = ds)
+summary(Mod3)
 
 # Test - Wichtigkeit der Freunde mit abhängigkeit der zufriedentheit 
 cor.test(q61_1~q1_3, data = ds)
@@ -134,44 +139,37 @@ xchisq.test(q61_1~q1_3, data = ds)
 
 # Lohnt sich mehr oder Weniger Arbeit - Frage q11_5
 tally(ds$q11_5)
-ds$MehrWenigerArbeit[ds$q11_5==1] <- "Hard Work sehr Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==2] <- "Hard Work sehr Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==3] <- "Hard Work Ziemlich Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==4] <- "Hard Work Ziemlich Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==5] <- "Hard Work Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==6] <- "Hard Work Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==7] <- "Hard Work Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==8] <- "Hard Work Nicht Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==9] <- "Hard Work Nicht Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==10] <- "Hard Work Nicht Wichtig"
-ds$MehrWenigerArbeit[ds$q11_5==98] <- "Neutral"
-ds$MehrWenigerArbeit[ds$q11_5==99] <- "N/A"
+ds$mehr_weniger_arbeit[ds$q11_5==1] <- "Hard Work sehr Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==2] <- "Hard Work sehr Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==3] <- "Hard Work Ziemlich Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==4] <- "Hard Work Ziemlich Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==5] <- "Hard Work Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==6] <- "Hard Work Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==7] <- "Hard Work Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==8] <- "Hard Work Nicht Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==9] <- "Hard Work Nicht Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==10] <- "Hard Work Nicht Wichtig"
+ds$mehr_weniger_arbeit[ds$q11_5==98] <- "Neutral"
+ds$mehr_weniger_arbeit[ds$q11_5==99] <- "N/A"
 
 # Sortieren der Werte
-ds$WichtigkeitFreizeit <- factor(ds$WichtigkeitFreizeit, levels = c ("Hard Work sehr Wichtig","Hard Work Ziemlich Wichtig","Hard Work Wichtig","Hard Work Nicht Wichtig","Neutral", "N/A"))
+ds$wichtigkeit_freizeit <- factor(ds$wichtigkeit_freizeit, levels = c ("Hard Work sehr Wichtig","Hard Work Ziemlich Wichtig","Hard Work Wichtig","Hard Work Nicht Wichtig","Neutral", "N/A"))
 
 # überprüfen
-tally(ds$MehrWenigerArbeit)
-typeof(ds$MehrWenigerArbeit)
+tally(ds$mehr_weniger_arbeit)
+typeof(ds$mehr_weniger_arbeit)
+
 
 # Graph - Mehr oder wienger Arbeit: 
-gf_counts(~ MehrWenigerArbeit, data = ds, fill = ~MehrWenigerArbeit, position = position_dodge() )
-gf_jitter(ZufriedenheitStatus~MehrWenigerArbeit, data = ds)
-gf_counts(~ ZufriedenheitStatus, data = ds, fill = ~ MehrWenigerArbeit, position = position_dodge())
-mosaicplot(ZufriedenheitStatus~MehrWenigerArbeit, data = ds)
-
-tally(MehrWenigerArbeit~ZufriedenheitStatus , data=ds, format = "percent")
-
-# Dataframe bereitstellen 
-DF_MehrWiengerArbeit_ZufriedenheitStatus <- tally(MehrWenigerArbeit ~ ZufriedenheitStatus, data=ds)
-print(DF_MehrWiengerArbeit_ZufriedenheitStatus)
-
-# Graph erstellen
-mosaicplot(DF_MehrWiengerArbeit_ZufriedenheitStatus)
-
+gf_counts(~ mehr_weniger_arbeit, data = ds, fill = ~mehr_weniger_arbeit, position = position_dodge() )
+gf_jitter(zufriedenheit_status~mehr_weniger_arbeit, data = ds)
+gf_counts(~ mehr_weniger_arbeit, data = ds, fill = ~ zufriedenheit_status, position = position_dodge())
+mosaicplot(zufriedenheit_status~mehr_weniger_arbeit, data = ds)
 
 # Test - 
+Mod4 <- aov(q61_1 ~ q11_5, data = ds)
+summary(Mod4)
 cor.test(q61_1~q11_5, data = ds)
 
-
+xchisq.test(zufriedenheit_status~mehr_weniger_arbeit, data = ds)
 
